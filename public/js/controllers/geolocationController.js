8cd3783a6a0d2af929aacfb89499e936f2196682
Levon <Levon.hakopyan@mail.ru>
2013-11-30 23:58:59 +0400

    try to finde way for js including

--- public/js/controllers/geolocationController.js
+++ public/js/controllers/geolocationController.js
@@ -1,38 +1,32 @@
-	var showMyLocation = function() {
-	
-		var onSuccess = function(position) {
-			var geolocation = {
-				Latitude: position.coords.latitude,
-				Longitude: position.coords.longitude,
-				Altitude: position.coords.altitude,
-				Accuracy: position.coords.accuracy,
-				AltitudeAccuracy: position.coords.altitudeAccuracy,
-				Heading: position.coords.heading,
-				Speed: position.coords.speed,
-				Timestamp: position.timestamp,
-			};
-		
-			$.ajax({
-				method: 'GET',
-				url: '/geolocation',
-				data: geolocation,
-				success: function(data) {
-					notify(data);
-				}
-			});
-			var notify = function(isSaved) {
-				if (isSaved) {				
-					alert('your geolocation information is saved to the server log file !');
-				}
-				else {
-					alert('your geolocation information is NOT SAVED !!!');
-				}
-			};
-		};
+var getLocation = function() {
 
-		var onError = function(error) {
-				alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-		}
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	var onSuccess = function(position) {
+		var geolocation = {
+			Latitude: position.coords.latitude,
+			Longitude: position.coords.longitude,
+			Altitude: position.coords.altitude,
+			Accuracy: position.coords.accuracy,
+			AltitudeAccuracy: position.coords.altitudeAccuracy,
+			Heading: position.coords.heading,
+			Speed: position.coords.speed,
+			Timestamp: position.timestamp,
+		};
+		
+		saveLocation(geolocation, function(isSaved) {
+			
+			if (isSaved) {				
+				alert('your geolocation information is saved to the server log file !');
+			}
+			else {
+				alert('your geolocation information is NOT SAVED !!!');
+			}
+
+		});
 	};
-	showMyLocation();
+
+	var onError = function(error) {
+			alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+	}
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+};
+
--- public/js/services/locationService.js
+++ public/js/services/locationService.js
@@ -1,16 +1,13 @@
-
-var locationService = {
-
-	saveLocation = function(geolocation, done) {
-		$.ajax({
-			method: 'GET',
-			url: '/location',
-			data: geolocation,
-			success: function(data) {
-				alert('your location is saved to the server log file !')
-			}
-		});
+{
+		saveLocation: function(geolocation, done) {
+			$.ajax({
+				method: 'GET',
+				url: '/location',
+				data: geolocation,
+				success: function(data) {
+					done(data);
+				}
+			});
+		}
 	};
-
 };
-
