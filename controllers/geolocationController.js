94e1629e1e9879b6d3e6733110ccfeacf57b32d6
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-11-30 19:18:34 +0100

    Update geolocationController.js

--- controllers/geolocationController.js
+++ controllers/geolocationController.js
@@ -5,6 +5,11 @@ exports.saveGeolocation = function(req, res) {
 	var geolocation = req.query;
 	
 	geolocationService.append(geolocation, function(isSaved) {
+		
+		// Of if you want to return boolean why not simply
+		// res.send(isSaved);
+		// you can use simply saved or even done
+		
 		if (isSaved) {
 			res.send(true);
 		}
