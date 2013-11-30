7ebab0967d9592077be56166bf9d0237758deb77
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-11-30 19:21:13 +0100

    Update geolocationService.js

--- services/geolocationService.js
+++ services/geolocationService.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
+
+// Can you export on object better than sunctions
+
 exports.append = function(geolocation, done) {
+
+	// I think i asked to symply log the location, why do you save to file? You mixed with the next work to save photo
 
 	fs.appendFile('log/geolocationLog.txt', 'Date: ' + new Date() +' '+ JSON.stringify(geolocation), function (err) {
 		if (err) throw err;
