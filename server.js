70370012530afa6fe11dc2fa1b0913e99987c1dd
Levon Hakopyan <levon.hakopyan@gmail.com>
2013-12-08 14:15:35 +0400

    reset

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 var express = require('express');
 var app = express();
-//var controller = require("/js/controller.js"); 
 
 var geolocationController = require('./controller/geolocationController');
 
 app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-<<<<<<< HEAD
-});
-
-//app.get("/location", controller.getLocation);
-=======
-  app.use(express.bodyParser());
+	app.use(express.static(__dirname + '/public'));
+	app.use('/lib', express.static(__dirname + '/bower_components'));
+	app.use(express.bodyParser());
 });
 
 app.post('/geolocation', geolocationController.log);
->>>>>>> Levon
 
 var port = 8080;
 app.listen(port);
--- service/geolocationService.js
+++ service/geolocationService.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 module.exports = {
 	
-	log: function(location, done) {
+	log: function(location, done) {	
 		fs.appendFile('log/locationLog.txt', this.formatLocationLog(location) , function(err) {
 			if (err) done(501);
 			done(200);
