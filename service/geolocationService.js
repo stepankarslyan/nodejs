e127d2910528a60453c8e9d9f1bb23e193021459
Levon <lev_hak@mail.ru>
2013-12-13 20:33:41 +0300

    Update geolocationService.js

--- service/geolocationService.js
+++ service/geolocationService.js
@@ -4,19 +4,8 @@ module.exports = {
 	
 	log: function(location, done) {	
 		console.log(location);
-		
-		// This is a mistake, the service don't care about http code, it should return an error or nothing
-		// To transfor an error to http code is responsability of controller, Service can be used from 
-		// another place than a webservice, what http code 200 will mean then?
-		// ex: done(); 
-		// and if error
-		// done("This is an error")
-		done(200);
-		
-		// Another solution is to add a config object with onSucces and onError
-		// config.onSucces()
-		// or 
-		// config.onError(error)
+		done();
+	
 	}
 	
 };
