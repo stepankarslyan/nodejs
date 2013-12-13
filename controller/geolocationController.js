6d53d29eb9b2a7de05c2db1ae3e49575e6b28b38
Levon <lev_hak@mail.ru>
2013-12-13 20:50:53 +0300

    Update geolocationController.js

--- controller/geolocationController.js
+++ controller/geolocationController.js
@@ -2,10 +2,10 @@ var geolocationService = require('../service/geolocationService');
 
 module.exports = {
 	
-	
+	log:function(req, res) {
+		var location = req.body.location;
 		geolocationService.log(location, function() {
 			res.send();
 		});	
 	}
-	
 };
