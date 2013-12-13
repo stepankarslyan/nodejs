2d87676a24232212a48e3fcc83c60b75e36d01c6
Levon <lev_hak@mail.ru>
2013-12-13 20:37:23 +0300

    Update geolocationController.js

--- controller/geolocationController.js
+++ controller/geolocationController.js
@@ -2,29 +2,9 @@ var geolocationService = require('../service/geolocationService');
 
 module.exports = {
 	
-	log: function(req, res) {
-		var location = JSON.parse(req.body.location);	
-		
-		// Status code do not come from service, the controller should decide to send 200 if success
-		// and 500 if error (an object with code and message for example)
-		// prefer something like
-		/*
-		geolocationService.log({
-			location: location,
-			
-			onSuccess: function() {
-				res.send();
-			},
-			
-			onError: function(error) {
-				res.statusCode = 500;
-				res.send(JSON.stringify(error));
-			}
-			
-		});
-		*/
-		geolocationService.log(location, function(statusCode) {
-			res.send(statusCode);
+	
+		geolocationService.log(location, function() {
+			res.send();
 		});	
 	}
 	
