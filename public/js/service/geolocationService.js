ccd118f933a02bebcf0163d56dfbb8f6e1387add
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-14 09:51:45 +0100

    Update geolocationService.js

--- public/js/service/geolocationService.js
+++ public/js/service/geolocationService.js
@@ -1,6 +1,6 @@
 app.geolocationService = {
 
-	getGeolocation: function(config) {
+	getGeolocation: function(config) { // Don't let the first line of code in function empty, there is no need to let empty line around a single line of code spread on some lines in file if there is only this line of code in the function
 	
 		navigator.geolocation.getCurrentPosition(
 			function(position) {
