7bb732fd5ecc3a558b0f2249aab8da05bb645425
Levon Hakopyan <levon.hakopyan@gmail.com>
2013-12-15 15:02:29 +0400

    correcting code

--- controller/cameraController.js
+++ controller/cameraController.js
@@ -1,11 +1,12 @@
 var cameraService = require('../service/cameraService');
+
 module.exports = {
+
 	save: function(req, res) {
-		var picture = req.body.picture;
-		
-		cameraService.save(picture, function(error) {	
+		cameraService.save(req.body.picture, function(error) {	
 			if(error) res.send(error);
 			res.send();
 		});
 	}
+	
 };
