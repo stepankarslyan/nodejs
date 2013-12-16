e3e635738d4a656412847c98f5ef3b2b697c1d98
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-16 11:20:47 +0100

    Update geolocationService.js

--- service/geolocationService.js
+++ service/geolocationService.js
@@ -4,7 +4,11 @@ module.exports = {
 	
 	log: function(location, done) {
 		fs.appendFile('log/locationLog.txt', this.formatLocationLog(location) , function(err) {
-			if (err) done(501);
+			// The server can be called out of a http request context, the http code 200 or 501 have no meaning
+			// for service, this is controller responsability
+			// service just tell that there was an error, controller decide what to do with error
+			// In general prefer the patern config with onSuccess and onError
+			if (err) done(501); // Don't use this syntax, use { ... }
 			done(200);
 		});
 	},
