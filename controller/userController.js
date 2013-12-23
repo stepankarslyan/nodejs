842ad3e20276e498507f517256ddbbce9faee100
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-23 09:55:17 +0100

    Update userController.js
    - Don't hide errors to people using your web services
    - Stay Rest using meaningfull http codes
    - Where is the login web service?
    - Ask lavo to show the pattern to delegate web service registration from server to controller

--- controller/userController.js
+++ controller/userController.js
@@ -4,6 +4,11 @@ module.exports = {
   
   add: function(req ,res) {
     var user = req.body.user;
+    
+    // Use the error management pattern we already used, transform errors in http codes, 200 and 500
+    // http code are even more precises, you cant use a code 200 that is created and put the id of the new creadte user in a 
+    // header property of http response search for it
+    
     userService.add(user, function(message){
       res.send(message);
     });
