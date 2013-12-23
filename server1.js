27093bd9a9a9c64d50c55ca718df1b0230581035
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-23 09:25:35 +0100

    Update server1.js
    Do what is asked, stop doing what you want and loosing your time on unnecessary things !!!

--- server1.js
+++ server1.js
@@ -1,3 +1,7 @@
+// Don't name files or variables with numers 1, 2, 3... give meaningfull names
+// Why do you create a new server file? Who told you to do this? You were supposed to create new web services not a new web server !!!
+// Be attentive to what is asked and do exactly what is asked. Programming is technical science that require precision !
+
 var express = require("express");
 var app = express();
 
