bd9111fdf7456c84a658f5c81aa808b0986f48eb
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-23 11:18:49 +0100

    Update cameraController.js
    Try to respect the Rest principle, work on resource, you are not adding a camera but a picture

--- controller/cameraController.js
+++ controller/cameraController.js
@@ -3,7 +3,11 @@ var cameraService = require('../service/cameraService');
 module.exports = {
 	
 	register: function(app) {
+		// Use Rest, you are not adding a camera but a picture
+		// It would be better to post on /pictures
 		app.post('/camera', this.save);
+		
+		// Add a function to get back a picture on url /pictures/id
 	},
 	
 	save: function(req, res) {
@@ -12,6 +16,8 @@ module.exports = {
 				res.statusCode = 500;
 				res.send(error);
 			}
+			
+			// Look the http codes, it's possible to put a create 200 status code and a property in response header telling a reference to the saved picture
 			res.send();
 		});
 	}
