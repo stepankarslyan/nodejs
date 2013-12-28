340bf871be2f2a8abc36e5de0e2580b3edfafb0a
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-28 18:20:36 +0100

    Update userLoginController.js

--- controller/userLoginController.js
+++ controller/userLoginController.js
@@ -1,14 +1,17 @@
 var userLoginService = require("../service/userLoginService");
 
+// Do not over divide your code, there is no need of one object for each method !
+// put this login method in user controller and use the userService !!!
+
 module.exports = {
 
   login: function(req, res) {
-    var user = req.query.user;
+    var user = req.query.user; // Don't use a local variable when not needed
     
-    userLoginService.login({
-    user: user,
+    userLoginService.login({ // You still make error of indentation !!!!
+    user: user, // This line and next ones should be one tab more on right
     
-    onSuccess: function(message) {
+    onSuccess: function(message) { // Don't send any message that is unusefull!! just use http 200 to tell all is ok
       res.send(message);
     },
     
