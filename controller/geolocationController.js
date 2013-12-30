6a95d9b3cc25aeb8d1f699ba832581140c91b623
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-30 18:19:54 +0100

    Update geolocationController.js

--- controller/geolocationController.js
+++ controller/geolocationController.js
@@ -3,13 +3,17 @@ var geolocationService = require('../service/geolocationService');
 module.exports = {
 	
 	register: function(app) {
+		// Register is registering empty function that simply call controller functions
+		// Don't put all code in function register that's not object  !!!
 		app.post('/geolocation', function(req, res) {
 		
-			geolocationService.log(req.body.position, function(error) {
+			geolocationService.log(req.body.position, function(error) { // No need for empty line before if it's first line of function and after if it's last line of function
+				// Prefer patern config with params and onSuccess and onFailure
+				
 				if(error) {
 					res.statusCode = 500;
 					res.send(error);
-				}
+				} // Put one else
 				res.send();
 			});
 				
