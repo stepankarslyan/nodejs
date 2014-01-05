64367ec28d554fef975b07f8902660dc6d8d3128
Levon Hakopyan <levon.hakopyan@gmail.com>
2014-01-05 22:36:33 +0400

    update camera and geolocatiion controllers

--- controller/cameraController.js
+++ controller/cameraController.js
@@ -3,44 +3,23 @@ var cameraService = require('../service/cameraService');
 module.exports = {
 	
 	register: function(app) {
-
-		app.post('/pictures', function(req, res) {
-			// The annonymous function must only be used to call the controller functions
-			// Not to put code in it !!!
-			// Imagine if you register 20 functions this will be 200 line that is unreadable !!!
-			cameraService.save(req.body.picture, function(error) {
-				if(error) {
-					res.statusCode = 500;
-					res.send(error);
-				}
-				
-				// Put a else
-				response.setHeader("Picture saved successly", 200); //it's this  you want ?
+		var controller = this;
+		app.post('/pictures', controller.save);
+	},
+	
+	save: function(req, res) {
+		cameraService.save(req.body.picture, function(error) {
+			if(error) {
+				res.statusCode = 500;
+				res.send(error);
+			}
+			else{
+				response.setHeader("Picture saved successly", 200);
 				// No need for message 200 means it's ok but there are http code 200 that are more precise
 				// 201, 202... search one with more meaning than simple 200
-				res.send();
-			});
-			
+				res.send();			
+			}
 		});
-		
-		// Don't let any comment, if you need you can look git history !!!
-		
-	/*
-	i let this code in comment because for now i dont have needed code in CameraService, and mongodb
-		app.get('/pictures/id', function(req, res) {
-			
-			cameraService.get(req.query.id, function(error, picture) {
-				if(error) {
-					res.setHeader("Picture not found", 404);
-					res.send(error)
-				}
-				else{
-					res.send(picture);
-				}
-			});
-			
-		});
-	*/
 	}
 	
 };
--- controller/geolocationController.js
+++ controller/geolocationController.js
@@ -3,21 +3,20 @@ var geolocationService = require('../service/geolocationService');
 module.exports = {
 	
 	register: function(app) {
-		// Register is registering empty function that simply call controller functions
-		// Don't put all code in function register that's not object  !!!
-		app.post('/geolocation', function(req, res) {
-		
-			geolocationService.log(req.body.position, function(error) { // No need for empty line before if it's first line of function and after if it's last line of function
-				// Prefer patern config with params and onSuccess and onFailure
-				
-				if(error) {
-					res.statusCode = 500;
-					res.send(error);
-				} // Put one else
-				res.send();
-			});
-				
-		});
+		var controller = this;
+		app.post('/geolocation', controller.log);
 	},
-
+	
+	log: function(req, res) {
+		geolocationService.log(req.body.position, function(error) {
+			if(error) {
+				res.statusCode = 500;
+				res.send(error);
+			}
+			else{
+				res.send();
+			}
+		});
+	}
+	
 };
